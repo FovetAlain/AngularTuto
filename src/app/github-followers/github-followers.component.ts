874e4ec3c25f28ec7257f12dd5dfc107106425f2
FovetAlain <fovetalain@gmail.com>
2018-05-23 12:54:48 +0200

    query params

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { GithubFollowersService } from './../services/github-followers.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: GithubFollowersService) { }
 
   ngOnInit() {
+    this.route.queryParamMap.subscribe(
+      params => {
+        console.log(params.get('order'));
+      }
+    );
     this.service.getAll()
       .subscribe(followers => this.followers = followers);
   }
